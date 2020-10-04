6c285f83141fb6b476c1dfe138aa725e401b1d76
hufy@akulaku.com <hufy@akulaku.com>
2020-10-04 21:49:49 +0800

    引入AppLifecycle插件

--- app/src/main/java/com/hfy/componentlearning/MyApplication.java
+++ app/src/main/java/com/hfy/componentlearning/MyApplication.java
@@ -3,6 +3,7 @@ package com.hfy.componentlearning;
 import android.app.Application;
 
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.hm.lifecycle.api.ApplicationLifecycleManager;
 
 public class MyApplication extends Application {
 
@@ -19,5 +20,29 @@ public class MyApplication extends Application {
         }
         // 尽可能早，推荐在Application中初始化
         ARouter.init(this);
+
+        ApplicationLifecycleManager.init();
+        ApplicationLifecycleManager.onCreate(this);
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+
+        ApplicationLifecycleManager.onTerminate();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+
+        ApplicationLifecycleManager.onLowMemory();
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+
+        ApplicationLifecycleManager.onTrimMemory(level);
     }
 }
